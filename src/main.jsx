29c32537171266c02ce35a1a8f2b28ff35d2a59a
Dinesh10731j <dinesh1122@gmail.com>
2024-06-14 13:18:45 +0545

    done

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
+
 
 const queryClient = new QueryClient();
 
@@ -11,7 +11,7 @@ const root = createRoot(container);
 
 root.render(
   <QueryClientProvider client={queryClient}>
-    <App />
-    <ReactQueryDevtools initialIsOpen={false} />
+    <App /> 
+
   </QueryClientProvider>
 );
